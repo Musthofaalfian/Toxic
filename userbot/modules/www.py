c6cc591961f3e755d3fa3944c579dac82346a5fb
Musthofaalfian <78151948+Musthofaalfian@users.noreply.github.com>
2021-03-10 04:53:49 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -85,17 +85,17 @@ async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("`KONTOLL....`")
+    await pong.edit("`DUARRRRR....`")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**NGENTOT!! 🐨**\n**KAMPANG** : %sms\n**Bot Uptime** : {uptime}🕛" % (duration))
+    await pong.edit(f"**JAMETTT!! 🚫**\n**KAMPANG** : %sms\n**Bot uptime** : {uptime}🕛" % (duration))
 
 
 @register(outgoing=True, pattern="^.pong$")
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     start = datetime.now()
-    await pong.edit("`GASPOL!!!`")
+    await pong.edit("`GASPOLLL!!!`")
     end = datetime.now()
     duration = (end - start).microseconds / 9000
     await pong.edit("`Ping!\n%sms`" % (duration))
