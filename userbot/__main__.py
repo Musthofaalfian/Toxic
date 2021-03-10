f9f0df6010203e1a683abcc7e3a77e2d990210ad
Musthofaalfian <78151948+Musthofaalfian@users.noreply.github.com>
2021-03-10 11:34:17 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"BOT-KAMPANG 🐨 V{BOT_VER} [MENYALA ANJEEEENG 🔥]")
+    f"🌹BOT-ANCIENT🌹 V{BOT_VER} [☢️ SUDAH MENYALA ☢️]")
 
 
 if len(argv) not in (1, 3, 4):
