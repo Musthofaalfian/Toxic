f235a765049bedc8bf06e8b9588195db60be5c80
Musthofaalfian <78151948+Musthofaalfian@users.noreply.github.com>
2021-03-20 16:29:07 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -88,14 +88,14 @@ async def pingme(pong):
     await pong.edit("`➥DORRRRR....`")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**🩸 Ping... 🩸** : %sms\n**➥ uptime** : {uptime} ‼️‼️‼️" % (duration))
+    await pong.edit(f"**🩸 Ping...** : %sms\n**➥  uptime** : {uptime} " % (duration))
 
 
 @register(outgoing=True, pattern="^.pong$")
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     start = datetime.now()
-    await pong.edit("`Tarik sisss‼️‼️‼️‼️‼️`")
+    await pong.edit("`Tarik sisss‼️‼️‼️‼️`")
     end = datetime.now()
     duration = (end - start).microseconds / 9000
     await pong.edit("`➥Ping!\n%sms`" % (duration))
