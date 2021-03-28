7618d9a0e2bf04611707efff730dbc3d432757d4
Musthofaalfian <78151948+Musthofaalfian@users.noreply.github.com>
2021-03-29 05:10:07 +0700

    Update oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -36,3 +36,39 @@ async def typewriter(typew):
     sleep(1)
     await typew.edit("`Suka Kegalakan Lu, Tapi Kok Sangat Bodohhh`")
 # Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.alfi(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("`Hai Perkenalkan Namaku {ALIVE_NAME}`")
+    sleep(3)
+    await typew.edit("`Maaf Baru Nimbrung`")
+    sleep(1)
+    await typew.edit("`Salam Kenal Yaa xixixi:)`")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.ayang(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("`Cuma Mau Bilang`")
+    sleep(3)
+    await typew.edit("`Aku Sayang Kamu`")
+    sleep(1)
+    await typew.edit("`I LOVE YOU 💞`")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.semangat(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("`Apapun Yang Terjadi`")
+    sleep(3)
+    await typew.edit("`Tetaplah Bernapas`")
+    sleep(1)
+    await typew.edit("`Dan Selalu Bersyukur`")
+# Create by myself @localheart
